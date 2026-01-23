aa0b5148ba59d4915a0fb100edf454e795ada044
yuma0000 <38005160+yuma0000@users.noreply.github.com>
2026-01-23 21:41:52 +0900

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -304,7 +304,7 @@ async def on_ready():
 if __name__ == "__main__":
     try:
         log.info("Starting Discord bot...")
-        bot.start(DISCORD_TOKEN)
+        bot.run(DISCORD_TOKEN)
     except KeyboardInterrupt:
         log.info("Bot manually stopped.")
     except Exception:
