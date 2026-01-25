369bfc55dc5636d2d809768ec16a72e7d7460bde
yuma0000 <38005160+yuma0000@users.noreply.github.com>
2026-01-25 14:43:03 +0900

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -16,7 +16,7 @@ from pathlib import Path
 import textwrap
 import math
 import subprocess
-import datetime
+from datetime import datetime
 
 from discord.ext import commands
 from discord import app_commands
