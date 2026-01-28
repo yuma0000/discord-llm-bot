7b181b16041f56603518b7ccbde87d662d75c751
yuma0000 <38005160+yuma0000@users.noreply.github.com>
2026-01-28 21:59:44 +0900

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -597,7 +597,7 @@ async def miq(interaction: discord.Interaction, text: discord.Message):
             {await make_hearts()}
             {await build_text_groups(lines, font_size)}
 
-             <text x="50%" y="{(lines * BASE_FONT_SIZE)}"
+             <text x="50%" y="{TOP_MARGIN + int(lines * font_size) + 50}"
                 text-anchor="middle"
                 dominant-baseline="middle"
                 font-size="{font_size * 0.7}"
