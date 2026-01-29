f0872f2f95ee72ff2ee43ac59f5c8d444d377d89
yuma0000 <38005160+yuma0000@users.noreply.github.com>
2026-01-29 23:50:42 +0900

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -122,7 +122,7 @@ SEARCH_RESULTS = ""
 signature = "まにまにあ"
 W, H = 500, 300
 CX, CY = W // 2, H // 2
-HEART = "♥️"
+HEART = "ゆ"
 TOP_MARGIN = 80
 BOTTOM_MARGIN = 80
 AVAILABLE_HEIGHT = H - TOP_MARGIN - BOTTOM_MARGIN
@@ -594,7 +594,11 @@ async def miq(interaction: discord.Interaction, text: discord.Message):
         <svg width="{W}" height="{H}" viewBox="0 0 {W} {H}"
             xmlns="http://www.w3.org/2000/svg">
 
-            <rect width="100%" height="100%" fill="#fff0f3"/>
+            h = random.random()
+            r, g, b = colorsys.hsv_to_rgb(h, s, v)
+            bg_color = f"rgb({int(r*255)}, {int(g*255)}, {int(b*255)})"
+            
+            <rect width="100%" height="100%" fill={bg_color}/> ##fff0f3
             {await make_hearts()}
             {await build_text_groups(lines, font_size)}
 
