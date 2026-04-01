69005628e750656c26c4a7d8e1be52c5c0f892f1
yuma0000 <38005160+yuma0000@users.noreply.github.com>
2026-04-01 17:00:11 +0900

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -131,8 +131,7 @@ BASE_FONT_SIZE = 24
 LINE_HEIGHT_RATE = 1.4
 MAX_LINES = math.floor(AVAILABLE_HEIGHT / (BASE_FONT_SIZE * LINE_HEIGHT_RATE))
 #FONT_NAME = "Noto Sans CJK JP, sans-serif"
-#FONT_NAME = "Plus50yen-de-TonJiru-ni-Henkoudekimasu"
-FONT_NAME = "Plus50yen"
+FONT_NAME = "Plus50yen-de-TonJiru-ni-Henkoudekimasu"
 
 s = 0.5
 v = 1.0
@@ -265,7 +264,7 @@ async def build_text_groups(lines, font_size):
                       y="{y}"
                       font-size="{font_size}"
                       font-family="{FONT_NAME}"
-                      font-weight="regular"
+                      font-weight="bold"
                       dominant-baseline="middle"
                       text-anchor="start">
                       {t}
@@ -729,7 +728,7 @@ async def miq(interaction: discord.Interaction, text: discord.Message):
                 dominant-baseline="middle"
                 font-size="{font_size * 0.7}"
                 font-family="{FONT_NAME}"
-                font-weight="regular"
+                font-weight="bold"
                 fill="#080808">
                 {text.author.display_name}
             </text>
